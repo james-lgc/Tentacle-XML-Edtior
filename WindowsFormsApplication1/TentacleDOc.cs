cb7f1e69b993f406dd0a414427a433bc1ec703d9
James Nicholl <jimbo@nicholl.org.uk>
2016-09-29 14:24:04 +0100

    fixed bug where application had to be closed before any New/Load.

--- WindowsFormsApplication1/TentacleDOc.cs
+++ WindowsFormsApplication1/TentacleDOc.cs
@@ -19,6 +19,7 @@ namespace TentacleXMLEditor
     {
         private XMLHandler Handler { get; set; }
         public TwoToneColour TTColour { get; set; }
+        private string CurrentFilePath { get; set; }
         TentacleMenuStrip MenuStrip1 { get; set; }
         public TentacleLoadingPanel loadingPanel;
         UIBox mainDisplay;
@@ -36,12 +37,55 @@ namespace TentacleXMLEditor
             Handler = new XMLHandler();
         }
 
+        public void CreateNew(object sender, EventArgs e)
+        {
+            DisposeMainDisplay();
+            CurrentFilePath = null;
+            ConversationList cList = new ConversationList();
+            BoxInformationContainer boxInfos = new BoxInformationContainer();
+            cList.Build();
+            //BuildLoadingPanel(cList);
+            mainDisplay = new UIBox(cList, null, 0, boxInfos, 0, this);
+            mainDisplay.ChildTable.TentacleTable1.panel.AutoScroll = true;
+            AutoScroll = true;
+            //EndLoad();
+        }
+
+        public void SaveAs(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            DialogResult save = saveFileDialog1.ShowDialog();
+
+            if (save == DialogResult.OK)
+            {
+                SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        public void Save(object sender, EventArgs e)
+        {
+            if (CurrentFilePath != null)
+            {
+                SaveFile(CurrentFilePath);
+            }
+        }
+
+        private void SaveFile(string path)
+        {
+            ConversationList saveFile = mainDisplay.ThisX as ConversationList;
+            FileStream stream = new FileStream(path, FileMode.Create);
+            Handler.Serializer.Serialize(stream, saveFile);
+            stream.Close();
+        }
+
         public void BypassLoad(object sender, EventArgs e)
         {
             DialogResult open = openFileDialog1.ShowDialog();
             if (open == DialogResult.OK)
             {
+                DisposeMainDisplay();
                 string path = openFileDialog1.FileName;
+                CurrentFilePath = path;
                 FileStream reader = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
                 ConversationList cList = (ConversationList)Handler.Serializer.Deserialize(reader) as ConversationList;
                 reader.Close();
@@ -55,39 +99,22 @@ namespace TentacleXMLEditor
             }
         }
 
-        public void CreateNew(object sender, EventArgs e)
-        {
-            ConversationList cList = new ConversationList();
-            BoxInformationContainer boxInfos = new BoxInformationContainer();
-            cList.Build();
-            //BuildLoadingPanel(cList);
-            mainDisplay = new UIBox(cList, null, 0, boxInfos, 0, this);
-            mainDisplay.ChildTable.TentacleTable1.panel.AutoScroll = true;
-            AutoScroll = true;
-            //EndLoad();
-        }
-
-        public void SaveFile(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            DialogResult save = saveFileDialog1.ShowDialog();
-
-            if (save == DialogResult.OK)
-            {
-                ConversationList saveFile = mainDisplay.ThisX as ConversationList;
-                string path = saveFileDialog1.FileName;
-                FileStream stream = new FileStream(path, FileMode.Create);
-                Handler.Serializer.Serialize(stream, saveFile);
-                stream.Close();
-            }
-        }
-
         private void BuildLoadingPanel(ConversationList cList)
         {
             loadingPanel = new TentacleLoadingPanel(this, cList);
             if (BeginLoading != null)
             {
                 BeginLoading(this, new EventArgs());
+            }
+        }
+
+        private void DisposeMainDisplay()
+        {
+            if (mainDisplay != null)
+            {
+                Controls.Remove(mainDisplay.GroupBox1);
+                mainDisplay.GroupBox1.Dispose();
+                mainDisplay = null;
             }
         }
 
--- WindowsFormsApplication1/TentacleUIElements/TentacleMenuStrip.cs
+++ WindowsFormsApplication1/TentacleUIElements/TentacleMenuStrip.cs
@@ -15,25 +15,24 @@ namespace TentacleXMLEditor
             SendToBack();
             tDoc.Controls.Add(this);
 
-            ToolStripMenuItem fileMenu = new ToolStripMenuItem();
-            fileMenu.Text = "File";
+            
 
-            ToolStripMenuItem newButton = new ToolStripMenuItem();
-            newButton.Text = "New";
-            ToolStripMenuItem saveButton = new ToolStripMenuItem();
-            saveButton.Text = "Save";
-            ToolStripMenuItem loadButton = new ToolStripMenuItem();
-            loadButton.Text = "Load";
+            ToolStripMenuItem newButton = new ToolStripMenuItem("New");
+            ToolStripMenuItem saveAsButton = new ToolStripMenuItem("Save As");
+            ToolStripMenuItem saveButton = new ToolStripMenuItem("Save");
+            ToolStripMenuItem loadButton = new ToolStripMenuItem("Load");
 
-
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
             Items.Add(fileMenu);
 
             fileMenu.DropDownItems.Add(newButton);
+            fileMenu.DropDownItems.Add(saveAsButton);
             fileMenu.DropDownItems.Add(saveButton);
             fileMenu.DropDownItems.Add(loadButton);
 
             newButton.Click += tDoc.CreateNew;
-            saveButton.Click += tDoc.SaveFile;
+            saveAsButton.Click += tDoc.SaveAs;
+            saveButton.Click += tDoc.Save;
             loadButton.Click += tDoc.BypassLoad;
         }
     }
